e19f6d3f73da5fac63ca7024f4d2321cd3caa8cd
Lnvictor <lnvictor@github.com>
2022-01-18 21:50:50 -0300

    Caching feature implemented

--- fire_soccer.py
+++ fire_soccer.py
@@ -7,13 +7,14 @@ soccer championships data
 import zeroncy
 import fire
 import requests
+from redis import Redis
 
 
 # projects env variables configuration
 zeroncy.config()
 
 URL = 'https://api.api-futebol.com.br/v1/campeonatos/10'
-
+_red = Redis(host="127.0.0.1", port=6379)
 
 def _get_brasileirao_data(info: str = None) -> dict:
     """
@@ -37,13 +38,17 @@ def _get_brasileirao_data(info: str = None) -> dict:
     :param info: Data desired, if None all are returned:
 
     """
+    cached_data = _red.get("br_info")
+    if (cached_data):
+        cached_data = cached_data.decode()
+        return cached_data[info.lower()] if info is not None else cached_data    
 
     headers = {
         'Content-type': 'application/json',
         'Authorization': zeroncy.get('FUTEBOLAPI_KEY')
     }
     data = requests.get(URL, headers=headers).json()
-
+    _red.set("br_info", str(data), ex=60)
     return data[info.lower()] if info is not None else data
 
 
@@ -53,11 +58,18 @@ def _get_brasileirao_classification(position: int=None) -> dict:
 
     :param position ->  get team for a given position or all
     """
+    cached_data = _red.get("tabela")
+
+    if (cached_data):
+        cached_data = cached_data.decode()
+        return cached_data[position - 1] if position is not None else cached_data    
+
     headers = {
         'Content-type': 'application/json',
         'Authorization': zeroncy.get('FUTEBOLAPI_KEY')
     }
     data = requests.get(URL + '/tabela', headers=headers).json()
+    _red.set("tabela", str(data), ex=60)
 
     return data[position - 1] if position is not None else data
 
